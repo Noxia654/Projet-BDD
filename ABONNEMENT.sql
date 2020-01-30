801c713529685666e65bc745e67256999dbb4cf9
Noxia654 <59877902+Noxia654@users.noreply.github.com>
2020-01-30 11:18:38 +0100

    Add files via upload

---
+++ ABONNEMENT.sql
@@ -0,0 +1,3 @@
+insert into ABONNEMENT (ID, TYPE_ABO, PRIX) values (1, 'HEBDOMADAIRE', '5.50€');
+insert into ABONNEMENT (ID, TYPE_ABO, PRIX) values (2, 'MENSUEL', '11.99€');
+insert into ABONNEMENT (ID, TYPE_ABO, PRIX) values (3, 'ANNUEL', '120.99€');
